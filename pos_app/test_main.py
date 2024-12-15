94a30b4bad0687fca77b2ff89d95d0f0c1c69d82
Gesare5 <gesare.nyangweso@gmail.com>
2024-12-15 08:52:22 +0300

    Change: Move test_main.py

---
+++ pos_app/test_main.py
@@ -0,0 +1,17 @@
+from pos_app.main import prelim_list, manage_coffee_items_list
+
+
+def test_prelim_list():
+    assert prelim_list() == [
+        "A: Order Coffee",
+        "B: Manage Coffee Items",
+        "C: Exit",
+    ]
+
+
+def test_manage_coffee_items_list():
+    assert manage_coffee_items_list() == [
+        ["1", "Add Coffee Item"],
+        ["2", "Delete Coffee Item"],
+        ["3", "View Coffee Items"],
+    ]
--- pos_app/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from pos_app.main import prelim_list
-
-
-def test_prelim_list():
-    assert prelim_list() == [
-        "A: Order Coffee",
-        "B: Manage Coffee Items",
-        "C: Exit",
-    ]
